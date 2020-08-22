3c5fd66587a23647267395662bb5f8e5ab07ec1e
stephanie mairena <stefyem7711@gmail.com>
2020-08-21 23:12:21 -0400

    MVP

--- api/server.js
+++ api/server.js
@@ -6,21 +6,77 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/api/accounts', (req,res) => {
-    db.()
+server.get('/api/accounts', async (req,res, next) => {
+   try {
+       const accounts = await db.select("*").from("accounts")
+
+    res.json(accounts)
+   } catch (err) {
+       next(err)
+   }
 })
 
-server.post('/api/accounts', (req,res) => {
-    
+server.get('/api/accounts/:id', async (req,res, next) => {
+    try {
+        const [account] = await db
+        .select("*")
+        .from("accounts")
+        .where("id", req.params.id)
+        .limit(1)
+ 
+     res.json(account)
+    } catch (err) {
+        next(err)
+    }
+ })
+server.post('/api/accounts', async (req,res,next) => {
+    try {
+        const [id] = await db
+            .insert({
+                name: req.body.name,
+                budget: req.body.budget,
+            })
+            .into("accounts")
+
+        const account = await db("accounts")
+            .where("id", id)
+            .first()
+
+            res.status(201).json(account)
+    } catch (err) {
+        next(err)
+    }
 })
 
-server.put('/api/accounts', (req,res) => {
-    db.('accounts').where({id: })
-    .update({name:})
+server.put('/api/accounts/:id', async (req,res,next) => {
+   try {
+       await db("accounts")
+        .update({
+            name: req.body.name,
+            budget: req.body.budget,
+        })
+        .where("id", req.params.id)
+
+        const account = await db("accounts")
+            .where("id", req.params.id)
+            .first()
+
+            res.json(account)
+   } catch (err) {
+       next(err)
+   }
 })
 
-server.delete('/api/accounts', (req,res) => {
-    db.('accounts').where().del();
+server.delete('/api/accounts/:id', (req,res,next) => {
+    try{
+        await db("accounts")
+            .where("id", req.params.id)
+            .del()
+
+            res.status(204).end()
+    } catch (err) {
+        next(err)
+    }
 })
 
 
